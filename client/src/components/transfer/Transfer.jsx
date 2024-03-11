b94a75c6df23f1218d558ca3ca2dcd7960ba8519
danielullauri93 <ullauriochoadaniel@gmail.com>
2024-03-11 14:54:05 -0500

    Transfer

--- client/src/components/transfer/Transfer.jsx
+++ client/src/components/transfer/Transfer.jsx
@@ -1,27 +1,86 @@
+import { FaUser } from "react-icons/fa";
 import "./transfer.css";
 
 const Transfer = () => {
   return (
     <div className="transferContainer">
-      <div className="transferContainerInputs">
+      <form className="transferContainerInputs">
         <div className="transferInputs">
-          <label htmlFor=""></label>
+          <label className="transferInputs-label">
+            <h3>Origen</h3>
+          </label>
+          <input
+            icon={<FaUser />}
+            type="text"
+            className="transferInput"
+            placeholder="Ingrese lugar de salida"
+          />
+        </div>
+        <div className="transferInputs">
+          <label className="transferInputs-label">
+            <h3>Destino</h3>
+          </label>
           <input
             type="text"
             className="transferInput"
-            placeholder="From"
+            placeholder="Ingrese lugar de destino"
           />
         </div>
-        <div className="transferInputs"></div>
-        <div className="transferInputs"></div>
-        <div className="transferInputs"></div>
-      </div>
-      <div className="transferContainerInputs">
-        <div className="transferInputs"></div>
-        <div className="transferInputs"></div>
-        <div className="transferInputs"></div>
-        <div className="transferInputs"></div>
-      </div>
+        <div className="transferInputs">
+          <label className="transferInputs-label">
+            <h3>Fecha de llegada</h3>
+          </label>
+          <input
+            type="text"
+            className="transferInput"
+            placeholder="Fecha de llegada"
+          />
+        </div>
+        <div className="transferInputs">
+          <label className="transferInputs-label">
+            <h3>Hora de llegada</h3>
+          </label>
+          <input
+            type="text"
+            className="transferInput"
+            placeholder="Hora de llegada"
+          />
+        </div>
+
+        <div className="transferInputs">
+          <label className="transferInputs-label">
+            <h3>Numero de personas</h3>
+          </label>
+          <input
+            type="text"
+            className="transferInput"
+            placeholder="Ingrese cuantas personas son"
+          />
+        </div>
+        <div className="transferInputs">
+          <label className="transferInputs-label">
+            <h3>Categoria</h3>
+          </label>
+          <input
+            type="text"
+            className="transferInput"
+            placeholder="Categoria"
+          />
+        </div>
+        <div className="transferInputs">
+          <label className="transferInputs-label">
+            <h3>Tipo de traslado</h3>
+          </label>
+          <input
+            type="text"
+            className="transferInput"
+            placeholder="Tipo de traslado"
+          />
+        </div>
+        <div className="transferInputs">
+          <button className="transferButton">Solicitar</button>
+        </div>
+      </form>
     </div>
   );
 };
